691286e21326373f8e5d80566ccd9b1164b90932
Shahad_Kadhim <almuhandisa.sh@gmail.com>
2022-08-09 14:18:47 +0300

    delete unused parameter

--- repositories/src/main/java/com/shahad/app/repositories/repositories/CreatorsRepositoryImp.kt
+++ repositories/src/main/java/com/shahad/app/repositories/repositories/CreatorsRepositoryImp.kt
@@ -4,23 +4,14 @@ package com.shahad.app.repositories.repositories
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.shahad.app.data.local.MarvelDao
-import com.shahad.app.data.mappers.LocalMappers
-import com.shahad.app.data.remote.MarvelService
-import com.shahad.app.data.toImageUrl
-import com.shahad.app.repositories.mappers.DomainMapper
-import com.shahad.app.repositories.convertTo
 import com.shahad.app.core.models.Creator
-import com.shahad.app.repositories.CharacterSource
+import com.shahad.app.data.remote.MarvelService
 import com.shahad.app.repositories.CreatorSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class CreatorsRepositoryImp @Inject constructor(
     private val api: MarvelService,
-    private val dao: MarvelDao,
-    private val domainMappers: DomainMapper,
-    private val localMappers: LocalMappers
 ): CreatorsRepository, BaseRepository {
 
 
--- repositories/src/main/java/com/shahad/app/repositories/repositories/StoriesRepositoryImp.kt
+++ repositories/src/main/java/com/shahad/app/repositories/repositories/StoriesRepositoryImp.kt
@@ -1,18 +1,12 @@
 package com.shahad.app.repositories.repositories
 
 import com.shahad.app.core.models.Story
-import com.shahad.app.data.local.MarvelDao
-import com.shahad.app.data.mappers.LocalMappers
 import com.shahad.app.data.remote.MarvelService
-import com.shahad.app.repositories.mappers.DomainMapper
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class StoriesRepositoryImp @Inject constructor(
     private val api: MarvelService,
-    private val dao: MarvelDao,
-    private val domainMappers: DomainMapper,
-    private val localMappers: LocalMappers
 ): StoriesRepository , BaseRepository {
 
     override fun getCharacterStories(characterId: Long): Flow<List<Story>?> {
